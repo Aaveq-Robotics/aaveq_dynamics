4f62c4714c7b7f66ee4dcbbc465e4da31889a7de
PositiveBeat <nicoline@finman.dk>
2023-11-20 14:14:34 +0100

    Reordered includes to follow convention

--- include/aaveq_dynamics/adynamics.hpp
+++ include/aaveq_dynamics/adynamics.hpp
@@ -1,3 +1,3 @@
-#include "aaveq_dynamics/utils.hpp"
-#include "aaveq_dynamics/rigid_body_dynamics.hpp"
 #include "aaveq_dynamics/forces.hpp"
+#include "aaveq_dynamics/rigid_body_dynamics.hpp"
+#include "aaveq_dynamics/utils.hpp"
